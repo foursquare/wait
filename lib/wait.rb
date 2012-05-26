f76fb2cc7d1f812c4153dcca20c453ae2aff40e7
Todd Mazierski <toddmazierski@gmail.com>
2012-05-26 14:25:21 -0400

    Wait.until is now an instance method, Wait#until. This was done so options can be set once and reused. Other changes: 1. The exception from the last attempt made is now raised 2. Simplified examples 3. Added Wait::TimeoutError (to not clash with Timeout::Error that may occur within the block) 4. Added more comments 5. Broke a few longer one-liners into separate lines

--- lib/wait.rb
+++ lib/wait.rb
@@ -1,93 +1,112 @@
 require 'timeout'
 require 'logger'
 
-# Executes a block until there's a result.
-#
-#   Wait.until { rand(2).zero? }
-#   => Rescued exception while waiting: Wait::Error: result was false
-#   => Attempt 1/5 failed, delaying for 1s
-#   => Rescued exception while waiting: Wait::Error: result was false
-#   => Attempt 2/5 failed, delaying for 2s
-#   => true
-#
-# By default, all exceptions are rescued.
-#
-#   Wait.until do
-#     if rand(2).zero?
-#       true
-#     else
-#       raise Exception
-#     end
-#   end
-#   => Rescued exception while waiting: Exception: Exception
-#   => Attempt 1/5 failed, delaying for 1s
-#   => true
-#
-# The attempt counter is passed to the block if special conditionals are
-# needed.
-#
-#   Wait.until(:attempts => 3) { |attempt| puts Time.now if attempt == 3 }
-#   => Rescued exception while waiting: Wait::Error: result was nil
-#   => Attempt 1/3 failed, delaying for 1s
-#   => Rescued exception while waiting: Wait::Error: result was nil
-#   => Attempt 2/3 failed, delaying for 2s
-#   => Sun Apr 29 10:03:17 -0400 2012
-#   => Rescued exception while waiting: Wait::Error: result was nil
-#   => Wait::Error: 3/3 attempts failed
-#
-# == Options
-#
-# [:attempts]
-#   Number of times to attempt the block. Default is 5.
-# [:timeout]
-#   Seconds the block is permitted to execute. Default is 15.
-# [:delay]
-#   Initial (grows exponentially) number of seconds to wait in between
-#   attempts. Default is 1.
-# [:exceptions]
-#   Array of exceptions to rescue. Default is +Exception+ (all exceptions).
-# [:debug]
-#   If +true+, logs debugging output. Default is +true+.
-#
-# == Returns
-#
-# The result of the block if:
-# * the result is not +nil+ or +false+
-# * the block doesn't raise an exception
-# * the block doesn't time out
-
 class Wait
 
-  def self.until(options = {})
-    attempts    = options[:attempts] || 5
-    timeout     = options[:timeout]  || 15
-    delay       = options[:delay]    || 1
-    exceptions  = options[:rescue]   || Exception
-    debug       = options[:debug]    || true
+  # Creates a new Wait instance.
+  #
+  # == Options
+  #
+  # [:attempts]
+  #   Number of times to attempt the block. Default is 5.
+  # [:timeout]
+  #   Seconds the block is permitted to execute. Default is 15.
+  # [:delay]
+  #   Initial (grows exponentially) number of seconds to wait in between
+  #   attempts. Default is 1.
+  # [:rescue]
+  #   One or an array of exceptions to rescue. Default is +Exception+ (all
+  #   exceptions).
+  # [:debug]
+  #   If +true+, logs debugging output. Default is +false+.
+  #
+  def initialize(options = {})
+    @attempts   = options[:attempts] || 5
+    @timeout    = options[:timeout]  || 15
+    @delay      = options[:delay]    || 1
+    @exceptions = options[:rescue]   || Exception
+    debug       = options[:debug]    || false
 
     # Prevent accidentally causing an infinite loop.
-    unless attempts.is_a?(Fixnum) && attempts > 0
+    unless @attempts.is_a?(Fixnum) && @attempts > 0
       raise ArgumentError, 'invalid number of attempts'
     end
 
-    logger = Logger.new(STDOUT)
-    logger.level = debug ? Logger::DEBUG : Logger::WARN
+    @logger = Logger.new(STDOUT)
+    @logger.level = debug ? Logger::DEBUG : Logger::WARN
+  end
 
-    # Initialize the attempt counter.
+  # == Description
+  #
+  # Executes a block until there's a result. Useful for blocking script
+  # execution until:
+  # * an HTTP request is successful
+  # * a port has opened
+  # * an external process has started
+  # * etc.
+  #
+  # == Examples
+  #
+  #   wait = Wait.new(:debug => true)
+  #   wait.until { rand(2).zero? }
+  #   => Rescued exception while waiting: Wait::NoResultError: result was false
+  #   => Attempt 1/5 failed, delaying for 1s
+  #   => Rescued exception while waiting: Wait::NoResultError: result was false
+  #   => Attempt 2/5 failed, delaying for 2s
+  #   => true
+  #
+  # By default, all exceptions are rescued (the +Exception+ class). However,
+  # the exception from the last attempt made is always raised.
+  #
+  #   wait = Wait.new(:debug => true, :attempts => 3)
+  #   wait.until do |attempt|
+  #     (attempt == 3) ? raise('raised!') : raise('rescued!')
+  #   end
+  #   => Rescued exception while waiting: RuntimeError: rescued!
+  #   => Attempt 1/3 failed, delaying for 1s
+  #   => Rescued exception while waiting: RuntimeError: rescued!
+  #   => Attempt 2/3 failed, delaying for 2s
+  #   => Rescued exception while waiting: RuntimeError: raised!
+  #   => RuntimeError: raised!
+  #
+  # == Returns
+  #
+  # The result of the block if not +nil+ or +false+.
+  #
+  # == Raises
+  #
+  # The exception from the last attempt made.
+  #
+  def until(&block)
+    # Initialize the attempt and delay counters.
     attempt = 0
+    delay = @delay
 
     begin
       attempt += 1
-      result = Timeout.timeout(timeout) { yield attempt }
-      result ? result : raise(Wait::Error, "result was #{result.inspect}")
-    rescue Wait::Error, *exceptions => exception
-      logger.debug "Rescued exception while waiting: #{exception.class.name}: #{exception.message}"
-      logger.debug exception.backtrace.join("\n")
 
-      if attempt == attempts
-        raise Wait::Error, "#{attempt}/#{attempts} attempts failed"
+      result = Timeout.timeout(@timeout, Wait::TimeoutError) do
+        # Execute the block and pass the attempt counter to it.
+        yield attempt
+      end
+
+      # If there's a result (neither +nil+ or +false+), return the result.
+      if result
+        result
       else
-        logger.debug "Attempt #{attempt}/#{attempts} failed, delaying for #{delay}s"
+        raise Wait::NoResultError, "result was #{result.inspect}"
+      end
+    rescue Wait::TimeoutError, Wait::NoResultError, *@exceptions => exception
+      @logger.debug 'Rescued exception while waiting: ' +
+        "#{exception.class.name}: #{exception.message}"
+      @logger.debug exception.backtrace.join("\n")
+
+      # If we've run out of attempts, raise the exception from the last
+      # attempt.
+      if attempt == @attempts
+        raise exception
+      else
+        @logger.debug "Attempt #{attempt}/#{@attempts} failed, delaying for #{delay}s"
         sleep delay
         delay *= 2
         retry
@@ -95,6 +114,10 @@ class Wait
     end
   end
 
-  class Error < StandardError; end
+  # Raised when a block doesn't return a result (+nil+ or +false+).
+  class NoResultError < StandardError; end
+
+  # Raised when a block times out.
+  class TimeoutError < Timeout::Error; end
 
 end #Wait
